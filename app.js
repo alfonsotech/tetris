20355be2b19db106a119a71ea00ecfc22c883eed
dratech <alfonsotech@nym.hush.com>
2017-01-19 14:29:27 -0800

    Implements rotation

--- app.js
+++ app.js
@@ -18,12 +18,19 @@ import * as Mousetrap from 'mousetrap'
 //   }
 //   return [new Model.O(1,1), new Model.L(1,4)];
 // }
+
 function reducer(state = new Model.Game(), action) {
   switch (action.type) {
     case 'TICK':
-      return state.tick();
+      const revedState = state.tick();
+      setTimeout(() => store.dispatch({type: 'TICK'}), 500);
+      return revedState;
     case 'ROTATE':
       return state.rotate();
+    case 'LEFT':
+      return state.left();
+    case 'RIGHT':
+      return state.right();
     default: return state;
   }
 }
@@ -48,7 +55,9 @@ store.subscribe(() => {
 var counter = 1;
 setInterval(() => store.dispatch({ type: 'TICK' }),500);
 
-Mousetrap.bind('space', function() { store.dispatch({type:'ROTATE'}); });
+Mousetrap.bind('space', () => store.dispatch({type:'ROTATE'}));
+Mousetrap.bind('left', () => store.dispatch({type:'LEFT'}));
+Mousetrap.bind('right', () => store.dispatch({type:'RIGHT'}));
 
 
 // //Tetronimo constructors
--- model.js
+++ model.js
@@ -1,4 +1,4 @@
-
+//Draws one square; adds func to re-draw square in new shape, drop one row, or keep in the same position
 export class Point {
   constructor(row,col) {
     this.row = row;
@@ -15,6 +15,7 @@ export class Point {
   }
 }
 
+//Draws/Re-draws shape in specific position
 export class Shape {
   constructor(name, rotator){
     this.name = name
@@ -28,6 +29,7 @@ export class Shape {
   }
 }
 
+//Throws tetronimos onto gameboard in initial offset location
 export class Piece {
   constructor(shape, offset = new Point(1, 10)) {
     this.shape = shape;
@@ -44,7 +46,8 @@ export class Piece {
     return Math.max.apply(null, this.points().map(point => point.col));
   }
   rotate() {
-    this.rotation = Piece.rotations()[(Piece.rotations().indexOf(this.rotation) + 1) % 4];
+    const rotations = Piece.rotations();
+    this.rotation = rotations[(rotations.indexOf(this.rotation) + 1) % 4];
   }
   hasPoint(point) {
     return this.points().some(item => item.sameAs(point));
@@ -59,7 +62,7 @@ export class Game {
    this.startAPiece();
    this.rows = 15;
    this.cols = 20;
-    this.rubble = [];
+   this.rubble = [];
   }
   tick() {
     this.fallingPiece.offset = this.fallingPiece.offset.fallOne();
@@ -87,26 +90,26 @@ export var shapes = {
    switch (rotation) {
      case 'N': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(4,1)];
      case 'E': return [new Point(2,1), new Point(2,2),new Point(2,3), new Point(2,4)];
-    case 'S': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(4,1)];
-    case 'W': return [new Point(2,1), new Point(2,2),new Point(2,3), new Point(2,4)];
+     case 'S': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(4,1)];
+     case 'W': return [new Point(2,1), new Point(2,2),new Point(2,3), new Point(2,4)];
       }
     }),
-    'T': new Shape('T', rotation => {
-      switch (rotation) {
-        case 'N': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(1,3)];
-        case 'E': return [new Point(1,2), new Point(2,2),new Point(3,2), new Point(2,1)];
-        case 'S': return [new Point(1,2), new Point(2,1),new Point(2,2), new Point(2,3)];
-        case 'W': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(2,2)];
-       }
-     }),
-   'L': new Shape('L', rotation => {
- switch (rotation) {
-    case 'N': return [new Point(1,1), new Point(2,1), new Point(1,2), new Point(1,3)];
-    case 'E': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(3,2)];
+  'T': new Shape('T', rotation => {
+    switch (rotation) {
+      case 'N': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(1,3)];
+      case 'E': return [new Point(1,2), new Point(2,2),new Point(3,2), new Point(2,1)];
+      case 'S': return [new Point(1,2), new Point(2,1),new Point(2,2), new Point(2,3)];
+      case 'W': return [new Point(1,1), new Point(2,1),new Point(3,1), new Point(2,2)];
+     }
+   }),
+ 'L': new Shape('L', rotation => {
+   switch (rotation) {
+     case 'N': return [new Point(1,1), new Point(2,1), new Point(1,2), new Point(1,3)];
+     case 'E': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(3,2)];
      case 'S': return [new Point(1,3), new Point(2,1), new Point(2,2), new Point(2,3)];
      case 'W': return [new Point(1,1), new Point(2,1), new Point(3,1), new Point(3,2)];
-   }
- }),
+    }
+  }),
  'Z': new Shape('Z', rotation => {
    switch (rotation) {
      case 'N': return [new Point(1,1), new Point(1,2), new Point(2,2), new Point(2,3)];
@@ -117,11 +120,11 @@ export var shapes = {
  })
 };
 
-// shapes.selectRandom = function() {
-//     var index = Math.floor(Math.random()*1000000%5);
-//     console.log('random index = ' + index);
-//     return shapes[Object.keys(shapes)[index]];
-//  }
+shapes.selectRandom = function() {
+    var index = Math.floor(Math.random()*1000000%5);
+    console.log('random index = ' + index);
+    return shapes[Object.keys(shapes)[index]];
+ }
 
 // export class Sqr {
 //   constructor(row, col) {
